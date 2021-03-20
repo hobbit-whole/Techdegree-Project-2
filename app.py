1ff7ed576f20572ec93e4d4b1dc5e410ad861593
hobbit-whole <80264493+hobbit-whole@users.noreply.github.com>
2021-03-19 21:20:00 -0600

    updation

--- app.py
+++ app.py
@@ -17,19 +17,9 @@ def unpack_player_name():
     return names
 
 def unpack_player_guardians():
-
-    _list_guardians = []
-
     guardians = [d['guardians'] for d in player_info if 'guardians' in d]
 
-    guardians = ', '.join(guardians).replace(" and", ",")
-
-    for item in guardians:
-        item = guardians.split(', ')
-        _list_guardians = item
-
-    
-    return _list_guardians
+    return guardians
 
 def unpack_player_height():
     height = [d['height'] for d in player_info if 'height' in d]
@@ -58,7 +48,7 @@ def create_dict(name, height, value, guardians):
 
     roster = defaultdict(list)
     for i in range(int(len(player_info))):
-        roster[i] = name[i], height[i], value[i]
+        roster[i] = name[i], height[i], value[i], guardians[i]
     #print(len(player_info))
     return roster
 
@@ -79,24 +69,22 @@ def random_sort(roster):
     return team_roster
 
 def assigned_to_team(roster_list):
+    team = defaultdict(list)
+    j = 0
+    x = 0
+    test = 0
     
-    print(roster_list[0][2], team_info[0])
-    length_player = int(len(roster_list)/len(team_info))
-    _random_list = defaultdict(list)
-    team = []
+    
+    for j in range(3):
+        team[j].append(team_info[j])
 
-    #for i in range(0, len(team_info)):
-    while j < 18:
-        for j in range(0,18):
-            if j <= 5 and j > 0:
-                print(roster_list[j])
-                if j < 11 and j >= 6:
-                    print(roster_list[j])
-                    if j < 18:
-                        print(roster_list[j])
-
-
-            
+        for i in range(6):     
+            while test != 3 and x != 3:
+                
+                if roster_list[i] not in team[j]:
+                    team[j].append(roster_list[i])
+                    print(team[j], roster_list)
+    return(team)
 
 
 
@@ -107,5 +95,46 @@ if __name__ == "__main__":
     guardians = unpack_player_guardians()
     roster = create_dict(name, height, value, guardians)
     game_time = random_sort(roster)
-    assigned_to_team(game_time)
+    sorted_roster = assigned_to_team(game_time)
+    dashes = "-"
     
+
+    '''team_number = int(input('\nHere are the teams\n\n 1) Panthers\n 2) Bandits\n 3) warriors\n\nEnter in an option:  '))
+    
+    if team_number <= 3:
+        print('{}\nYou selected: {}\n'.format(dashes*25, sorted_roster[team_number-1][0]))
+        print('Total Players: {}'.format(len(sorted_roster[team_number-1])-1))
+
+        experience_list = []
+        for y in range(1, len(sorted_roster[team_number-1])):
+            #print(sorted_roster[team_number-1][y])
+            experience_list.append(sorted_roster[team_number-1][y][2])
+        true_count = sum(experience_list)
+        false_count = 6 - true_count
+        #print(true_count)
+
+        print('Experienced Players: {}'.format(true_count))
+        print('Inexperienced Player: {}'.format(false_count))
+
+
+
+        _list_of_height = []
+        for i in range(1, len(sorted_roster[team_number-1])):
+            height_list = []
+            height_list.append(sorted_roster[team_number-1][i])
+            for z in range(0, len(height_list)):
+                _list_of_height.append(height_list[z][1])
+        _average_player = round(statistics.mean(_list_of_height), 1)
+        print('Average Height: {}\n'.format(_average_player))
+
+        names = []
+        for i in range(1, len(sorted_roster[team_number-1])):
+            names.append(sorted_roster[team_number-1][i][0])
+            names_of_players = ', '.join(names)
+        print('Players on the team :\n\t{}\n'.format(names_of_players))
+
+        guardian_for_player = []
+        for i in range(1, len(sorted_roster[team_number-1])):
+            guardian_for_player.append(sorted_roster[team_number-1][i][3])
+            names_of_guardians = ', '.join(guardian_for_player).replace(" and", ",")
+        print('Guardians: \n\t{}\n'.format(names_of_guardians))'''
