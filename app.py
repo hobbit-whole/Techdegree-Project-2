62be11a89786cb61ee38fee94ebd1addff4b10ed
hobbit-whole <80264493+hobbit-whole@users.noreply.github.com>
2021-03-24 10:43:42 -0600

    All finished

--- app.py
+++ app.py
@@ -6,27 +6,23 @@ import random, statistics
 player_info = []
 team_info = []
 
-for player in PLAYERS:
-    player_info.append(player)
-
-for team in TEAMS:
-    team_info.append(team)
 
 
 
-def unpack_player_name():
+
+def unpack_player_name(player_info):
     names = [d['name'] for d in player_info if 'name' in d]
     #print(names)
     return names
 
-def unpack_player_guardians():
+def unpack_player_guardians(player_info):
     guardians = [d['guardians'] for d in player_info if 'guardians' in d]
 
     #print(guardians)
     return(guardians)
 
 
-def unpack_player_height():
+def unpack_player_height(player_info):
     height = [d['height'] for d in player_info if 'height' in d]
     high = []
     for i in range(0,len(height)):
@@ -35,7 +31,7 @@ def unpack_player_height():
     #print(high)
     return high
 
-def unpack_player_experience():
+def unpack_player_experience(player_info):
     experience = [d['experience'] for d in player_info if 'experience' in d]
     value = []
     for i in range(len(experience)): 
@@ -49,7 +45,7 @@ def unpack_player_experience():
     return(value)
 
 
-def create_dict(name, height, value, guardians):
+def create_dict(player_info, name, height, value, guardians):
 
     roster = defaultdict(list)
 
@@ -71,7 +67,7 @@ def populate_team(roster_list):
 
     return team_roster
 
-def assigned_to_team(roster_list):
+def assigned_to_team(roster_list, team_info):
 
     team = defaultdict(list)
     picked_player= []
@@ -112,64 +108,96 @@ def assigned_to_team(roster_list):
 
 
 if __name__ == "__main__":
-    name = unpack_player_name()
-    height = unpack_player_height()
-    value = unpack_player_experience()
-    guardians = unpack_player_guardians()
-    roster = create_dict(name, height, value, guardians)
-    roster_list = populate_team(roster)
-    sorted_roster = assigned_to_team(roster_list)
+    for player in PLAYERS:
+        player_info.append(player)
 
+    for team in TEAMS:
+        team_info.append(team)
+
+    name = unpack_player_name(player_info)
+    height = unpack_player_height(player_info)
+    value = unpack_player_experience(player_info)
+    guardians = unpack_player_guardians(player_info)
+    roster = create_dict(player_info, name, height, value, guardians)
+    roster_list = populate_team(roster)
+    sorted_roster = assigned_to_team(roster_list, team_info)
+
+    menu_response = 0
     dashes = "-"
     _continue_choice = ''
 
+    while menu_response != 1 and menu_response != 2:
+        print('\nBASKETBALL TEAM STATS TOOL')
 
-    while _continue_choice.lower() != 'n':    
-        team_number = int(input('\nHere are the teams\n\n 1) Panthers\n 2) Bandits\n 3) warriors\n\nEnter in an option:  '))
+        print('\n{}MENU{}'.format(dashes*10, dashes*10))
+
         
-        if team_number <= 3:
-            print('{}\nYou selected: {}\n'.format(dashes*25, sorted_roster[team_number-1][0]))
-            print('Total Players: {}'.format(len(sorted_roster[team_number-1])-1))
-
-            experience = 0
-            inexperience = 0
-            for i in range(1, len(sorted_roster[team_number-1])):
-                if sorted_roster[team_number-1][i][2] == True:
-                    experience += 1
-                else:
-                    inexperience += 1
-
-            print('Experienced Players: {}'.format(experience))
-            print('Inexperienced Player: {}'.format(inexperience))
+        try:
+            menu_response = int(input('\nHere are your choices\n\n 1) Display Team Stats\n 2) Quit\n\nEnter in an option: '))
+        except ValueError:
+            print('Please choose between 1 and 2')
+                
+        except NameError:
+            print('Please choose between 1 and 2')
 
 
+        while menu_response == 1 and _continue_choice.lower() != 'n':  
+            try:  
+                team_number = int(input('\nHere are the teams\n\n 1) Panthers\n 2) Bandits\n 3) warriors\n\nEnter in an option:  '))
 
-            _list_of_height = []
-            for i in range(1, len(sorted_roster[team_number-1])):
-                height_list = []
-                height_list.append(sorted_roster[team_number-1][i])
-                for z in range(len(height_list)):
-                    _list_of_height.append(height_list[z][1])
-            _average_player = round(statistics.mean(_list_of_height), 1)
-            print('Average Height: {} inches\n{}'.format(_average_player, dashes*25))
 
-            names = []
-            for i in range(1, len(sorted_roster[team_number-1])):
-                names.append(sorted_roster[team_number-1][i][0])
-                names_of_players = ', '.join(names)
-            print('Players on the team :\n\t{}\n'.format(names_of_players))
+                if team_number <= 3 and team_number != 0:
+                    print('{}\nYou selected: {}\n'.format(dashes*25, sorted_roster[team_number-1][0]))
+                    print('Total Players: {}'.format(len(sorted_roster[team_number-1])-1))
+                
 
-            guardian_for_player = []
-            for i in range(1, len(sorted_roster[team_number-1])):
-                guardian_for_player.append(sorted_roster[team_number-1][i][3])
-                names_of_guardians = ', '.join(guardian_for_player).replace(" and", ",")           
-            print('Guardians: \n\t{}\n'.format(names_of_guardians))
+                    experience = 0
+                    inexperience = 0
+                    for i in range(1, len(sorted_roster[team_number-1])):
+                        if sorted_roster[team_number-1][i][2] == True:
+                            experience += 1
+                        else:
+                            inexperience += 1
 
-        _continue_choice = input('{}\nWould you like to pick another team (y/n): '.format(dashes*25))
-        
-        if _continue_choice == 'n':
-            print('{}\nExiting Program \n'.format(dashes*25))
-            break
+                    print('Experienced Players: {}'.format(experience))
+                    print('Inexperienced Player: {}'.format(inexperience))
+
+
+
+                    _list_of_height = []
+                    for i in range(1, len(sorted_roster[team_number-1])):
+                        height_list = []
+                        height_list.append(sorted_roster[team_number-1][i])
+                        for z in range(len(height_list)):
+                            _list_of_height.append(height_list[z][1])
+                    _average_player = round(statistics.mean(_list_of_height), 1)
+                    print('Average Height: {} inches\n{}'.format(_average_player, dashes*25))
+
+                    names = []
+                    for i in range(1, len(sorted_roster[team_number-1])):
+                        names.append(sorted_roster[team_number-1][i][0])
+                        names_of_players = ', '.join(names)
+                    print('Players on the team :\n\t{}\n'.format(names_of_players))
+
+                    guardian_for_player = []
+                    for i in range(1, len(sorted_roster[team_number-1])):
+                        guardian_for_player.append(sorted_roster[team_number-1][i][3])
+                        names_of_guardians = ', '.join(guardian_for_player).replace(" and", ",")           
+                    print('Guardians: \n\t{}\n'.format(names_of_guardians))
+
+            except ValueError:
+                print('Please enter in a number 1-3')
+                
+            except NameError:
+                print('Please enter in a number between 1-3')
+
+            
+            _continue_choice = input('{}\nWould you like to pick another team (y/n): '.format(dashes*25))
+
+            menu_response = 0
+    
+    else:
+        print('{}\nExiting Program \n'.format(dashes*25))
 
 
         
